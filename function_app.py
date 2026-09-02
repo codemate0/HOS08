c6f9617a74eec49b3e6e34f9fddc31351f8c5880
Manas Teja Katakam <katakammanasteja@cityuniversity.edu>
2026-09-01 19:51:22 -0700

    Delete function_app.py

--- function_app.py
+++
@@ -1,7 +0,0 @@
-import azure.functions as func
-
-app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
-
-@app.route(route="hello")
-def hello(req):
-    return func.HttpResponse("Hello")
